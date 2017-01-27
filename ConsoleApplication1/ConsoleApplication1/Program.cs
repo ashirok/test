c0072e0089dd0558313e8a2143653124cc31f2cc
ashirok <ashirok@rambler.ru>
2017-01-27 14:46:06 +0300

    Добавление файла по С++

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет");
+            String name = Console.ReadLine();
+            if (name == "Саша")
+            {
+                Console.WriteLine("Привет " + name);
+            } else
+            {
+                string other = name;
+                Console.WriteLine("Привет " + other);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
